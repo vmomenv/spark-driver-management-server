3908ab1de0ece935dcf0e1b38f0536d4948a311f
momen <vmomenv@gmail.com>
2024-02-18 11:57:45 +0800

    添加token生成

--- spark-sever/main.py
+++ spark-sever/main.py
@@ -12,7 +12,11 @@ from fastapi import File, UploadFile
 from fastapi.responses import JSONResponse
 from pathlib import Path
 from typing import Set
-
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
 import os
 import json
 # 创建 FastAPI 应用
@@ -39,7 +43,94 @@ default_engine = create_engine(default_db_uri)
 if not database_exists(default_engine.url):
     create_database(default_engine.url)
 
+# 读取secretkey
+secret_config = configparser.ConfigParser()
+config.read('secretkey.ini')
+# 获取数据
+SECRET_KEY = config['secretkey']['secret_key']
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# 创建密码哈希上下文
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# 创建用于验证token的OAuth2PasswordBearer对象
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 metadata = MetaData()
+
+# 登录接口
+@app.post("/token")
+
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": form_data.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+# 验证token的函数
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    return username
+
+# 生成token的函数
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# 根据用户名和密码验证用户
+def authenticate_user(username: str, password: str):
+    # 在数据库中查询用户信息
+    db_user = get_user_by_username(username)
+
+    # 校验密码是否匹配
+    if db_user and verify_password(password, db_user['password']):
+        print("pipei")
+        return db_user
+    print("bpipei")
+    return None
+
+# 在数据库中根据用户名获取用户信息
+def get_user_by_username(username: str):
+    db = SessionLocal()
+    try:
+        query = db.query(users).filter(users.c.username == username).first()
+        if query:
+            return {
+                "username": query.username,
+                "password": query.password,
+                "role_id": query.role_id
+            }
+    finally:
+        db.close()
+
+# 校验密码是否匹配
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
 # 创建数据库表
 users = Table(
